fc3fa5a0ed5bc50cbd32dfab97994084d18454e6
Ashackq <akashpatelyo2@gmail.com>
2025-03-25 17:04:07 +0530

    [update]
    button and bread

--- src/screens/testakash.tsx
+++ src/screens/testakash.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Buttonnew } from "../components";
 const Testakash = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Buttonnew />
+    </div>
+  );
 };
 
 export default Testakash;
